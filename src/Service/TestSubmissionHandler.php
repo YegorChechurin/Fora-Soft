efc1915ced3372149c2b67ec854259b20699eaaf
Yegor Chechurin <yegor.chechurin@gmail.com>
2019-06-14 15:53:22 +0300

    created

---
+++ src/Service/TestSubmissionHandler.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Test;
+use App\Entity\SubmittedTest;
+use App\Entity\SubmittedAnswer;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class TestSubmissionHandler 
+{
+	public function buildForm(FormBuilderInterface $builder, Test $test)
+}
